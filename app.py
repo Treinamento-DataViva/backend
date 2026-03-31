4f88250ad86ac5d869873c10a449693b341b20d7
Ana Clara <anaclara2002araujo@gmail.com>
2026-03-31 11:10:27 -0300

    feat: integra backend ao frontend via API

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ app = FastAPI(title="Censo Escolar API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+
+
 
 app.include_router(escola_router, prefix="/escolas", tags=["Escolas"])
 
@@ -19,3 +26,8 @@ app.include_router(escola_router, prefix="/escolas", tags=["Escolas"])
 @app.get("/")
 def root():
     return {"message": "API do Censo Escolar funcionando com sucesso"}
+
+
+@app.get("/teste")
+def teste():
+    return {"mensagem": "Backend funcionando"}
