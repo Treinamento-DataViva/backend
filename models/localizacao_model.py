9c89609d99e37606501cf16b4bebc1ca318a3bdf
Paulo Henrique Carmona Ramos <paulo.carmona17@gmail.com>
2026-03-31 12:32:42 -0300

    feat: altera o tipo dos dados de latitude e longitude

--- models/localizacao_model.py
+++ models/localizacao_model.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, Float, Text
+from geoalchemy2 import Geometry
 from db.connection import Base
 
 class Localizacao(Base):
     __tablename__ = "localizacao"
 
     id_escola = Column(Integer, primary_key=True, index=True)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    geometry = Column(Text, nullable=True)
+    latitude = Column(Text, nullable=True)
+    longitude = Column(Text, nullable=True)
+    geometry = Column(Geometry(geometry_type="POINT",srid= 4326), nullable=True)
